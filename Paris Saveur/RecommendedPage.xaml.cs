1ba5f721461ab2e44b5827c527dd4f5337df480e
zelong <zelong.chen@applidget.com>
2015-01-06 14:28:24 +0100

    add recommended page layout

--- Paris Saveur/RecommendedPage.xaml.cs
+++ Paris Saveur/RecommendedPage.xaml.cs
@@ -13,31 +13,27 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=391641 上有介绍
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkID=390556 上有介绍
 
 namespace Paris_Saveur
 {
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class RecommendedPage : Page
     {
-        public MainPage()
+        public RecommendedPage()
         {
             this.InitializeComponent();
-
-            this.NavigationCacheMode = NavigationCacheMode.Required;
         }
 
+        /// <summary>
+        /// 在此页将要在 Frame 中显示时进行调用。
+        /// </summary>
+        /// <param name="e">描述如何访问此页的事件数据。
+        /// 此参数通常用于配置页。</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-
-        }
-
-
-        private void recommendedText_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(RecommendedPage));
         }
     }
 }

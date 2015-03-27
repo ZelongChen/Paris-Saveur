9ceb86956448efae644fe171fe660b6663257ef0
zelong <zelong.chen@applidget.com>
2015-03-27 23:29:43 +0100

    use new icons

--- Paris Saveur/MainPage.xaml.cs
+++ Paris Saveur/MainPage.xaml.cs
@@ -31,12 +31,6 @@ namespace Paris_Saveur
             this.InitializeComponent();
 
             this.NavigationCacheMode = NavigationCacheMode.Required;
-            ImageDownloader.DownloadImageIntoImage(recommendedImage, "https://pbs.twimg.com/profile_images/1550954462/instagramIcon_400x400.png");
-            ImageDownloader.DownloadImageIntoImage(nearbyImage, "http://cdn.marketplaceimages.windowsphone.com/v8/images/b821d889-4d99-42dc-b711-0f93c63e192a?imageType=ws_icon_large");
-            ImageDownloader.DownloadImageIntoImage(hotImage, "http://static1.squarespace.com/static/50dcd3bfe4b0395512971edc/t/54346f07e4b0ebb220d2199b/1412722441352/Instagram.jpg");
-            ImageDownloader.DownloadImageIntoImage(sortbyImage, "http://i2.cdn.turner.com/money/dam/assets/141219073832-bieber-instagram-620xa.jpg");
-            ImageDownloader.DownloadImageIntoImage(hottagImage, "http://cdns2.freepik.com/free-photo/_408-64283.jpg");
-            ImageDownloader.DownloadImageIntoImage(metroImage, "http://s3-static-ak.buzzfed.com/static/2014-06/30/11/campaign_images/webdr10/now-kylie-jenner-has-been-accused-of-photoshoppin-2-13413-1404140944-12_big.jpg");
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)

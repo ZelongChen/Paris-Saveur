40735e6a6ef45ac4c52dcd64ea6b92744f2ecaa2
zelong <zelong.chen@applidget.com>
2015-04-12 17:59:44 +0200

    add alphagrouped list

---
+++ Paris Saveur/Model/TransportStationList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paris_Saveur.Model
+{
+    class TransportStationList
+    {
+        public ObservableCollection<TransportStation> TransportStations { get; private set; }
+        public List<AlphaKeyGroup<TransportStation>> AlphaGrouped { get; private set; }
+
+        public TransportStationList()
+        {
+            TransportStations = new ObservableCollection<TransportStation>();
+            AddStations();
+            GetAlphaKeyGroup();
+        }
+
+        private void AddStations()
+        {
+            TransportStation station = new TransportStation();
+            station.Name = "AAAAA";
+            TransportStations.Add(station);
+
+            station = new TransportStation();
+            station.Name = "BBBBB";
+            TransportStations.Add(station);
+        }
+
+        private void GetAlphaKeyGroup()
+        {
+            var alphaKeyGroup = AlphaKeyGroup<TransportStation>.CreateGroups(
+                TransportStations,
+                (TransportStation s) => { return s.Name; },
+                true);
+            AlphaGrouped = alphaKeyGroup.ToList();
+        }
+    }
+}
---
+++ Paris Saveur/TransportStationsPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Paris_Saveur.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkID=390556 上有介绍
+
+namespace Paris_Saveur
+{
+    public sealed partial class TransportStationsPage : Page
+    {
+        public TransportStationsPage()
+        {
+            this.InitializeComponent();
+            TransportStationList list = new TransportStationList();
+
+            this.DataContext = list;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}

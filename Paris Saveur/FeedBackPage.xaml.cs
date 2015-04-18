72ec0defd2ce8fdea7737e4f40aaec2e900ffaee
zelong <zelong.chen@applidget.com>
2015-04-18 22:29:05 +0200

    clean feedback page

--- Paris Saveur/FeedBackPage.xaml.cs
+++ Paris Saveur/FeedBackPage.xaml.cs
@@ -16,13 +16,9 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Web.Http;
 
-// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkID=390556 上有介绍
-
 namespace Paris_Saveur
 {
-    /// <summary>
-    /// 可用于自身或导航至 Frame 内部的空白页。
-    /// </summary>
+
     public sealed partial class FeedBackPage : Page
     {
         public FeedBackPage()
@@ -39,30 +35,40 @@ namespace Paris_Saveur
             this.LoadingRing.IsActive = true;
             this.LoadingRing.Visibility = Visibility.Visible;
 
-            String feedbackContent = this.FeedBackTextBox.Text;
-            Dictionary<string, string> fullHttpContentDictionary = new Dictionary<string,string>();
-            fullHttpContentDictionary.Add("device_name", "WindowsPhone");
-            Windows.Security.ExchangeActiveSyncProvisioning.EasClientDeviceInformation deviceInfo = new Windows.Security.ExchangeActiveSyncProvisioning.EasClientDeviceInformation();
-            string firmwareVersion = deviceInfo.SystemFirmwareVersion.ToString();
-            fullHttpContentDictionary.Add("device_info", firmwareVersion);
-            fullHttpContentDictionary.Add("app_name", "v0.8WP");
-            fullHttpContentDictionary.Add("app_version", "1.0");
-            fullHttpContentDictionary.Add("comment", feedbackContent);
-            HttpFormUrlEncodedContent formContent = new HttpFormUrlEncodedContent(fullHttpContentDictionary);
+            HttpFormUrlEncodedContent formContent = SetupHttpFormUrlEncodedContent();
             HttpClient client = new HttpClient();
             Uri uri = new Uri("http://www.vivelevendredi.com/feedbacks/submit/mobile/");
             HttpResponseMessage response = await client.PostAsync(uri, formContent);
             if (response.IsSuccessStatusCode)
             {
-                XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
-                XmlNodeList elements = toastXml.GetElementsByTagName("text");
-                elements[0].AppendChild(toastXml.CreateTextNode("感谢您的反馈"));
-                ToastNotification toast = new ToastNotification(toastXml);
-                ToastNotificationManager.CreateToastNotifier().Show(toast);
-                this.FeedBackTextBox.Text = "";
+                SetupToastNotification();
             }
             this.LoadingRing.IsActive = false;
             this.LoadingRing.Visibility = Visibility.Collapsed;
+        }
+
+        private HttpFormUrlEncodedContent SetupHttpFormUrlEncodedContent()
+        {
+            String feedbackContent = this.FeedBackTextBox.Text;
+            Dictionary<string, string> fullHttpContentDictionary = new Dictionary<string, string>();
+            fullHttpContentDictionary.Add("device_name", "WindowsPhone");
+            Windows.Security.ExchangeActiveSyncProvisioning.EasClientDeviceInformation deviceInfo = new Windows.Security.ExchangeActiveSyncProvisioning.EasClientDeviceInformation();
+            string firmwareVersion = deviceInfo.SystemFirmwareVersion.ToString();
+            fullHttpContentDictionary.Add("device_info", firmwareVersion);
+            fullHttpContentDictionary.Add("app_name", "WP");
+            fullHttpContentDictionary.Add("app_version", "1.1.0.3");
+            fullHttpContentDictionary.Add("comment", feedbackContent);
+            return new HttpFormUrlEncodedContent(fullHttpContentDictionary);
+        }
+
+        private void SetupToastNotification()
+        {
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+            XmlNodeList elements = toastXml.GetElementsByTagName("text");
+            elements[0].AppendChild(toastXml.CreateTextNode("感谢您的反馈"));
+            ToastNotification toast = new ToastNotification(toastXml);
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+            this.FeedBackTextBox.Text = "";
         }
     }
 }

afa6ea27b56813dde506c7eae47108e042434d70
zelong <zelong.chen@applidget.com>
2015-05-01 15:55:19 +0200

    reload all references

--- Paris Saveur/MainPage.xaml.cs
+++ Paris Saveur/MainPage.xaml.cs
@@ -32,7 +32,6 @@ namespace Paris_Saveur
         public MainPage()
         {
             this.InitializeComponent();
-            //AdMediator_ErrorHandler();
             this.NavigationCacheMode = NavigationCacheMode.Required;
         }
 
@@ -150,39 +149,5 @@ namespace Paris_Saveur
         {
             Frame.Navigate(typeof(SearchPage));
         }
-/*
-        private void AdMediator_ErrorHandler()
-        {
-            AdMediator_C43C32.AdSdkError += AdMediator_Bottom_AdError;
-            AdMediator_C43C32.AdMediatorFilled += AdMediator_Bottom_AdFilled;
-            AdMediator_C43C32.AdMediatorError += AdMediator_Bottom_AdMediatorError;
-            AdMediator_C43C32.AdSdkEvent += AdMediator_Bottom_AdSdkEvent;
-        }
-
-        // and then add these functions
-
-        void AdMediator_Bottom_AdSdkEvent(object sender, Microsoft.AdMediator.Core.Events.AdSdkEventArgs e)
-        {
-            Debug.WriteLine("AdSdk event {0} by {1}", e.EventName, e.Name);
-        }
-
-        void AdMediator_Bottom_AdMediatorError(object sender, Microsoft.AdMediator.Core.Events.AdMediatorFailedEventArgs e)
-        {
-            Debug.WriteLine("AdMediatorError:" + e.Error + " " + e.ErrorCode);
-            // if (e.ErrorCode == AdMediatorErrorCode.NoAdAvailable)
-            // AdMediator will not show an ad for this mediation cycle
-        }
-
-        void AdMediator_Bottom_AdFilled(object sender, Microsoft.AdMediator.Core.Events.AdSdkEventArgs e)
-        {
-            Debug.WriteLine("AdFilled:" + e.Name);
-        }
-
-        void AdMediator_Bottom_AdError(object sender, Microsoft.AdMediator.Core.Events.AdFailedEventArgs e)
-        {
-            Debug.WriteLine("AdSdkError by {0} ErrorCode: {1} ErrorDescription: {2} Error: {3}", e.Name, e.ErrorCode, e.ErrorDescription, e.Error);
-        }
- **/
-
     }
 }

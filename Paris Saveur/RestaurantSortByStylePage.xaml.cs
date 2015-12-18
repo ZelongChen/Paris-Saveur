5c4cb4cfe82804cff3ccfee9e88c1424516197f8
zelong <zelong.chen@applidget.com>
2015-12-18 16:37:27 +0100

    clean style page

--- Paris Saveur/RestaurantSortByStylePage.xaml.cs
+++ Paris Saveur/RestaurantSortByStylePage.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
+﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 
@@ -28,47 +17,47 @@ namespace Paris_Saveur
         {
         }
 
-        private void sichuan_hunan_Tapped(object sender, TappedRoutedEventArgs e)
+        private void Sichuan_Hunan_Tapped(object sender, TappedRoutedEventArgs e)
         {
             Frame.Navigate(typeof(HotRestaurantPage), "Sichuan_Hunan");
         }
 
-        private void shandong_anhui_Tapped(object sender, TappedRoutedEventArgs e)
+        private void Shandong_Anhui_Tapped(object sender, TappedRoutedEventArgs e)
         {
             Frame.Navigate(typeof(HotRestaurantPage), "Shandong_Anhui");
         }
 
-        private void jiangshu_zhejiang_Tapped(object sender, TappedRoutedEventArgs e)
+        private void Jiangshu_Zhejiang_Tapped(object sender, TappedRoutedEventArgs e)
         {
             Frame.Navigate(typeof(HotRestaurantPage), "Jiangsu_Zhejiang");
         }
 
-        private void guangdong_fujian_Tapped(object sender, TappedRoutedEventArgs e)
+        private void Guangdong_Fujian_Tapped(object sender, TappedRoutedEventArgs e)
         {
             Frame.Navigate(typeof(HotRestaurantPage), "Cantonese_Fujian");
         }
 
-        private void yunnan_Tapped(object sender, TappedRoutedEventArgs e)
+        private void Yunnan_Tapped(object sender, TappedRoutedEventArgs e)
         {
             Frame.Navigate(typeof(HotRestaurantPage), "Yunnan");
         }
 
-        private void beifang_Tapped(object sender, TappedRoutedEventArgs e)
+        private void Beifang_Tapped(object sender, TappedRoutedEventArgs e)
         {
             Frame.Navigate(typeof(HotRestaurantPage), "Northern_Chinese");
         }
 
-        private void japan_korea_Tapped(object sender, TappedRoutedEventArgs e)
+        private void Japan_Korea_Tapped(object sender, TappedRoutedEventArgs e)
         {
             Frame.Navigate(typeof(HotRestaurantPage), "Japanese_Korean");
         }
 
-        private void southest_asia_Tapped(object sender, TappedRoutedEventArgs e)
+        private void Southest_Asia_Tapped(object sender, TappedRoutedEventArgs e)
         {
             Frame.Navigate(typeof(HotRestaurantPage), "South_Asian");
         }
 
-        private void unclassified_Tapped(object sender, TappedRoutedEventArgs e)
+        private void Unclassified_Tapped(object sender, TappedRoutedEventArgs e)
         {
             Frame.Navigate(typeof(HotRestaurantPage), "Unclassified");
         }
